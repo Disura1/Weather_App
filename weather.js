83703cae000851e9d674d557dcb0e92b3a801c42
Disura Sandaruwan <disurasandaruwan0@gmail.com>
2026-03-31 19:04:50 +0530

    Functions completed

--- weather.js
+++ weather.js
@@ -9,19 +9,34 @@ fetch(url)
     document.getElementById(
       "city"
     ).innerText = `${data.name}, ${data.sys.country}`;
-    document.getElementById(
-      "temp"
-    ).innerText = `🌡️ Temperature: ${data.main.temp} °C`;
-    document.getElementById(
-      "desc"
-    ).innerText = `🌤️ ${data.weather[0].description}`;
-    const iconCode = data.weather[0].icon;
+    document.getElementById("main").innerText = data.weather[0].main;
+    document.getElementById("description").innerText =
+      data.weather[0].description;
     document.getElementById(
       "icon"
-    ).src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-    document.getElementById("icon").alt = data.weather[0].main;
+    ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    document.getElementById("temp").innerText = data.main.temp;
+    document.getElementById("feels_like").innerText = data.main.feels_like;
+    document.getElementById("temp_min").innerText = data.main.temp_min;
+    document.getElementById("temp_max").innerText = data.main.temp_max;
+    document.getElementById("humidity").innerText = data.main.humidity;
+    document.getElementById("pressure").innerText = data.main.pressure;
+    document.getElementById("visibility").innerText = data.visibility;
+
+    document.getElementById("wind_speed").innerText = data.wind.speed;
+    document.getElementById("wind_deg").innerText = data.wind.deg;
+    document.getElementById("clouds").innerText = data.clouds.all;
+
+    const toTime = (timestamp) => {
+      const date = new Date(timestamp * 1000);
+      return date.toLocaleTimeString();
+    };
+
+    document.getElementById("sunrise").innerText = toTime(data.sys.sunrise);
+    document.getElementById("sunset").innerText = toTime(data.sys.sunset);
   })
   .catch((error) => {
-    document.getElementById("city").innerText = "Error loading weather 😢";
-    console.error(error);
+    document.getElementById("city").innerText = "Error loading weather";
+    console.error("Error:", error);
   });
